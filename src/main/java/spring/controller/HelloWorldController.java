775de8302627c27d50029d85f4ce1f5d933790c0
Kuba <j.antoniak8@gmail.com>
2018-03-17 17:26:10 +0100

    trol

--- src/main/java/spring/controller/HelloWorldController.java
+++ src/main/java/spring/controller/HelloWorldController.java
@@ -9,6 +9,6 @@ public class HelloWorldController {
     @RequestMapping("/")
     public String helloWorld()
     {
-        return "Hello world! ;) ;P";
+        return "Hello world! ;) ;PasaA";
     }
 }
