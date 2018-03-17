276c4368bf84e34ee7aaeb06291271ada829d1e8
Kuba <j.antoniak8@gmail.com>
2018-03-17 14:05:00 +0100

    Added simple Rest controller

---
+++ src/main/java/spring/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String helloWorld()
+    {
+        return "Hello world!";
+    }
+}
