fd756893eb4e16176411f2ebe1d5c838393144ec
Kuba <j.antoniak8@gmail.com>
2018-04-02 09:39:06 +0200

    meaningless commit, let's have some fun with jenkins + 2

--- src/main/java/spring/controller/HelloWorldController.java
+++ src/main/java/spring/controller/HelloWorldController.java
@@ -9,6 +9,6 @@ public class HelloWorldController {
     @RequestMapping("/")
     public String helloWorld()
     {
-        return "Hello world! ;) Just a few more commits to go!";
+        return "Hello world! ;) Just a few more commits to go! Testing new deployments!";
     }
 }
