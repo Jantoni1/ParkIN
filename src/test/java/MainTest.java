42ff347752569d2483506d5a5fe16d2cb76e7daa
Mikolaj Umanski <umaan96@gmail.com>
2018-04-03 15:49:28 +0200

    Added sample SpringBoot context test #PSM-12

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,15 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import spring.controller.Main;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Main.class)
 public class MainTest {
+
+    @Test
+    public void contextLoads() throws Exception {
+    }
+
 }
