83b27ec3c6000f52be9f43521cc90885b07a4b30
Kuba <j.antoniak8@gmail.com>
2018-05-18 06:35:52 +0200

    Changed name of controller #PSM-37

---
+++ src/main/java/com/parkin/HTMLProviderController.java
@@ -0,0 +1,24 @@
+package com.parkin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HTMLProviderController {
+
+    @RequestMapping("/")
+    public String indexHTML() {
+        return "index";
+    }
+
+    @RequestMapping("/tariff")
+    public String tariffHTML() {
+        return "tariff";
+    }
+
+    @RequestMapping("/stats")
+    public String statsHTML() {
+        return "stats";
+    }
+
+}
