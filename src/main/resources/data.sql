d89be28d2b7b713fc0bafd0ede0f961f8e76eeb7
Urbanov <dawidurbanowicz@outlook.com>
2018-05-20 21:14:38 +0200

    Deleted unnecessary schema

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into data.tariffs values(0, 10, 2, 8) on conflict do nothing;
-insert into data.configurations values(0, 'capacity', 20) on conflict do nothing;
+insert into tariffs values(0, 10, 2, 8) on conflict do nothing;
+insert into configurations values(0, 'capacity', 20) on conflict do nothing;
