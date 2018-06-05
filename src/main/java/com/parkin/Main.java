3b6e34dbec154551aa878b1ac24de181e97c267f
Kuba <j.antoniak8@gmail.com>
2018-06-05 12:06:57 +0200

    added test coverage check

--- src/main/java/com/parkin/Main.java
+++ src/main/java/com/parkin/Main.java
@@ -3,6 +3,10 @@ package com.parkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/***
+ * @brief main class of the application, starts spring context
+ */
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into tariffs values(0, 10, 2, 8);
-insert into configurations values(0, 'capacity', 20);
+insert into tariffs values(0, 10, 2, 8) on conflict do nothing;
+insert into configurations values(0, 'capacity', 20) on conflict do nothing;
