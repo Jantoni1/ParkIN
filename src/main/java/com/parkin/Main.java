0ad54932125f7b2a4a7b5f73072f10f79a0a061c
Kuba <j.antoniak8@gmail.com>
2018-06-05 12:12:32 +0200

    added test coverage check

--- src/main/java/com/parkin/Main.java
+++ src/main/java/com/parkin/Main.java
@@ -3,12 +3,15 @@ package com.parkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-/***
- * @brief main class of the application, starts spring context
+/**
+ * main class 
  */
 @SpringBootApplication
 public class Main {
+
+    /***
+     *  main class of the application, starts spring context
+     */
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
